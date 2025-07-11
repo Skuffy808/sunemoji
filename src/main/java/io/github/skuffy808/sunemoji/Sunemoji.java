f38cb4588ce2afd986f466fb64cb95dcf5d22fc2
Skuffy <skuffy808@gmail.com>
2025-07-11 18:08:59 +1200

    add onEnable log

---
+++ src/main/java/io/github/skuffy808/sunemoji/Sunemoji.java
@@ -0,0 +1,18 @@
+package io.github.skuffy808.sunemoji;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sunemoji extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Plugin loaded!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
